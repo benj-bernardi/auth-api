b5082a43894d696b094ae168bab31914adda10b4
benj-bernardi <benjaminbernardi33@gmail.com>
2026-01-26 22:58:21 -0300

    feat: setup express app with static files, user routes and global error handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import userRoutes from "./routes/user.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/users", userRoutes);
 app.use(errorHandler);
 

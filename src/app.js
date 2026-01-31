bdfb1d377d2d92bd0bd553a0180e06d8120fbe1a
benj-bernardi <benjaminbernardi33@gmail.com>
2026-01-30 23:17:51 -0300

    feat: add admin routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import { fileURLToPath } from "url";
 import { errorHandler } from "./middlewares/errorHandler.js";
+
 
 const app = express();
 
@@ -12,6 +14,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/users", userRoutes);
+app.use("/admin", adminRoutes);
 app.use(errorHandler);
 
 export default app;

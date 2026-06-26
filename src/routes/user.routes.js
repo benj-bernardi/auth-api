f83e2d0328c442fdbbe47ac6f98185321baca890
benj-bernardi <benjaminbernardi33@gmail.com>
2026-06-25 23:19:55 -0300

    feat: add ratelimit middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getMe, registerUser, loginUser, updateUser, deleteUser } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { loginLimiter } from "../middlewares/rateLimit.js";
 
 const router = Router();
 
 // Auth Routes
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", loginLimiter, loginUser);
 
 // User Routes
 router.get("/me", authMiddleware, getMe);

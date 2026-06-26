7ff0bf962989d02a6ceab1c1a1a9bb24dec8ff9c
benj-bernardi <benjaminbernardi33@gmail.com>
2026-06-25 23:21:04 -0300

    feat: add ratelimit middleware

---
+++ src/middlewares/rateLimit.js
@@ -0,0 +1,11 @@
+import { ratelimit } from "express-rate-limit";
+
+export const loginLimiter = rateLimit({
+    windowMs: 15* 60 * 1000,
+    max: 10,
+    message: {
+        error: "Too many login attempts, try again later."
+    },
+    standartHeaders: true,
+    legacyHeaders: true
+});

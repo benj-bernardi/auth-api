7b093d65ea53c8d6c736e36ace61816eb0d080ae
benj-bernardi <benjaminbernardi33@gmail.com>
2026-07-08 23:21:31 -0300

    chore: ajust windowMs

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,7 +1,7 @@
-import { ratelimit } from "express-rate-limit";
+import ratelimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
-    windowMs: 15* 60 * 1000,
+    windowMs: 15 * 60 * 1000,
     max: 10,
     message: {
         error: "Too many login attempts, try again later."

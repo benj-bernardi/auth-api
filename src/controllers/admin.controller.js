8234d3ce277c127a8879944de58a96f32233d9f8
benj-bernardi <benjaminbernardi33@gmail.com>
2026-07-11 22:51:22 -0300

    fix: fixing admin routes

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,27 +1,26 @@
 import { getAllUsers, deleteUserByID } from "../models/admin.model.js";
 
-export async function getUsers(req, res, next){
+export async function getUsers(req, res, next) {
     try {
-        const result = await getAllUsers();
-        res.json(result);
-    } catch (err){
+        const users = await getAllUsers();
+        res.json(users);
+    } catch (err) {
         next(err);
     }
 }
 
-export async function deleteUser(req, res, next){
+export async function deleteUser(req, res, next) {
     try {
         const { id } = req.params;
 
-        const deleteUserbyID = await deleteUserByID(id);
+        const deleted = await deleteUserByID(id);
 
-        if (!deleteUserbyID){
+        if (!deleted) {
             return res.status(404).json({ error: "User not found" });
         }
-    
+
         res.status(204).send();
-    } catch (err){
+    } catch (err) {
         next(err);
     }
 }
-

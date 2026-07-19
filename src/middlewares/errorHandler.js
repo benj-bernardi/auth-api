e2f0fc5f3f4560873fc157b132bb2dc60e660131
benj-bernardi <benjaminbernardi33@gmail.com>
2026-07-19 20:26:09 -0300

    chore: ,

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,6 +5,6 @@ export function errorHandler(err, req, res, next) {
   const message = err.message || "Internal Server Error";
 
   res.status(statusCode).json({
-    error: message,
+    error: message
   });
 }
